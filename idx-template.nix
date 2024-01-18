604891bd55095ab8f61c785e607483dde285af84
Julien Martel <julien@monogram.io>
2024-01-17 19:33:55 -0600

    test refactor

--- idx-template.nix
+++ idx-template.nix
@@ -1,12 +1,14 @@
-# /opt/nix/store/pi1a3ca85v80whh6pyanpv1lf3idm6h7-idx-template/bin/idx-template ~/Monospace/workspace/nix_templates/lanchain-js-gemini --output-dir ~ --workspace-name foo -a '{"typescript": "true"}'
 { pkgs, typescript ? false, ... }: {
-    packages = [
-      pkgs.nodejs
-    ];
+    packages = [];
     bootstrap = ''
+      cp -rf ${if typescript then ./ts else ./js } "$out"
 
-      cp -rf ${if typescript then ./ts else ./js } "$WS_NAME"
-      chmod -R +w "$WS_NAME"
-      mv "$WS_NAME" "$out"
+      mkdir "$out"/.idx
+      cp ${./shared/dev.nix} "$out"/.idx
+      cp ${./shared/hotdog.jpg} "$out"
+      cp ${./shared/.gitignore} "$out"
+      cp ${./shared/.example.env} "$out"/.env
+
+      chmod -R +w "$out"
     '';
 }
