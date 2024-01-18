d82900425a6d224c554bb8949ff569bdf7a83d98
Julien Martel <julien@monogram.io>
2024-01-17 20:03:50 -0600

    test refactor again

--- idx-template.nix
+++ idx-template.nix
@@ -1,14 +1,15 @@
 { pkgs, typescript ? false, ... }: {
     packages = [];
     bootstrap = ''
-      cp -rf ${if typescript then ./ts else ./js } "$out"
+      cp -rf ${if typescript then ./ts else ./js } "$WS_NAME"
 
-      mkdir "$out"/.idx
-      cp -f ${./shared/dev.nix} "$out"/.idx/
-      cp ${./shared/hotdog.jpg} "$out"/hotdog.jpg
-      cp -f ${./shared/gitignore} "$out"/.gitignore
-      cp -f ${./shared/example.env} "$out"/.env
+      mkdir "$WS_NAME"/.idx
+      cp ${./shared/dev.nix} "$WS_NAME"/.idx/
+      cp ${./shared/hotdog.jpg} "$WS_NAME"/hotdog.jpg
+      cp ${./shared/gitignore} "$WS_NAME"/.gitignore
+      cp ${./shared/example.env} "$WS_NAME"/.env
 
+      mv "$WS_NAME" "$out"
       chmod -R +w "$out"
     '';
 }
