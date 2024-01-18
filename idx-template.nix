6066c1d83bca05ff3850d363a05a32d10420425d
Julien Martel <julien@monogram.io>
2024-01-17 20:01:01 -0600

    test refactor again

--- idx-template.nix
+++ idx-template.nix
@@ -4,10 +4,10 @@
       cp -rf ${if typescript then ./ts else ./js } "$out"
 
       mkdir "$out"/.idx
-      cp ${./shared/dev.nix} "$out"/.idx/dev.nix
+      cp -f ${./shared/dev.nix} "$out"/.idx/
       cp ${./shared/hotdog.jpg} "$out"/hotdog.jpg
-      cp ${./shared/gitignore} "$out"/.gitignore
-      cp ${./shared/example.env} "$out"/.env
+      cp -f ${./shared/gitignore} "$out"/.gitignore
+      cp -f ${./shared/example.env} "$out"/.env
 
       chmod -R +w "$out"
     '';
